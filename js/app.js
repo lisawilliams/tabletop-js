13a630da92e361a651ed9e02bc65623d4eb2b7e9
lisawilliams <lisa@placeblogger.com>
2013-04-22 14:46:41 -0400

    Solved undefined error
    …but now we have a new error -- Tabletop can't seem to find the Google
    spreadsheet, and gives a 400: Bad Request error on tabletop.js line 184.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
 $(document).ready(function () {
   Tabletop.init({
-    key: 'YOUR GOOGLE SPREADSHEET KEY HERE',
+  
+  // key only: 0AppPPX-L4ABKdExWaVBqWFFuVnFYU0Y1OHUwc0dNb3c'
+  // full public url to spreadsheet: https://docs.google.com/spreadsheet/ccc?key=0AppPPX-L4ABKdExWaVBqWFFuVnFYU0Y1OHUwc0dNb3c&usp=sharing
+  // currently this gives a 400:Bad Request error on 
+  // GET https://spreadsheets.google.com/feeds/worksheets/0AppPPX-L4ABKdExWaVBqWFFuV…/basic?alt=json-in-script&callback=Tabletop.callbacks.tt136665610463249112 400 (Bad Request) 
+  // at tabletop.js line 184
+  
+    key: '0AppPPX-L4ABKdExWaVBqWFFuVnFYU0Y1OHUwc0dNb3c',
     callback: function(data, tabletop) { 
       var i,
           dataLength = data.length;
